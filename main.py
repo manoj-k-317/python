a56f0354904c6f51df867afd8e188dd01f3914be
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-15 00:39:00 +0530

    math fns updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #name = "manoj"     #str
 #age = 21           #int
 #cgpa = 8.1         #float
-#arrears =          #False #boolean
+#arrears = False    #boolean
 
 #-type casting-
 #print(type(age))       #to check the datatype
@@ -20,10 +20,18 @@
 #number %= 3        #modulus
 #number **=2        #exponent(power)
 
-#-math funtions-   
-#result = round(num)            #round off 
+#-math funtions- part 1 
+#result = round(num)            #round off
 #result = abs(num)              #absolute value
 #result = pow(num, 2)           #power
 #result = min(num1,num2,num3)   #minimum
 #result = max(num1,num2,num3)   #maximum
 
+#-math funtions- part 2
+#import math                    #module import
+#print(math.pi)                 #pi value
+#print(math.e)                  #exponential constant value
+#print(math.sqrt(144))          #square root
+#print(math.ceil(7.5))          #round up
+#print(math.floor(7.5))         #round down
+#print(round(circumstance, 3))  #round & cut off certain digits after point (1.23456789 -> 1.234)
