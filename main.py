982ec8e9abbe7a07b81423b80e64af9f765f15fc
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-15 00:16:46 +0530

    math fns

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 #print("hello python!")
 
 #-variables-
-#name = "manoj" #str
-#age = 21 #int
-#cgpa = 8.1 #float
-#arrears = False #boolean
+#name = "manoj"     #str
+#age = 21           #int
+#cgpa = 8.1         #float
+#arrears =          #False #boolean
 
 #-type casting-
-#print(type(age)) #to check the datatype
-#age = float(age) #to change the datatype
+#print(type(age))       #to check the datatype
+#age = float(age)       #to change the datatype
 #print(type(age))
 
 #-arithmetic operations-
-#number = 18
-#number += 3
-#number -= 3
-#number *= 3
-#number /= 3
-#number //= 3
-#number %= 3
+#number = 5
+#number += 3        #addition
+#number -= 3        #subtraction
+#number *= 3        #multipilication
+#number /= 3        #division
+#number %= 3        #modulus
+#number **=2        #exponent(power)
+
+#-math funtions-   
+#result = round(num)            #round off 
+#result = abs(num)              #absolute value
+#result = pow(num, 2)           #power
+#result = min(num1,num2,num3)   #minimum
+#result = max(num1,num2,num3)   #maximum
 
