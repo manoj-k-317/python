d8e7323415bb57c2e6a404ae914562af148abb9d
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-21 22:34:55 +0530

    string indexinh exc

--- exc.py
+++ exc.py
@@ -1,13 +1,5 @@
 #this file exixts in sub branch
 
-name = str(input("Enter your name please:"))
+cc_num = "0123-4567-8901-2345"
 
-lenght = len(name)
-if lenght > 12:
-    print("name should not exceed 12 letters")
-
-num_or_space = name.isalpha()
-if num_or_space and lenght<=12:
-    print("validation is completed")
-elif num_or_space == False:
-    print("your name should not contains space of number!, try again")
+print(cc_num[::-1])
