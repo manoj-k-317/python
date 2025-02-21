de65b128514f0693d0a55bc895666d436f23568c
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-21 21:45:31 +0530

    user validation prgm

--- exc.py
+++ exc.py
@@ -1,1 +1,13 @@
-#this file exixts in main branch
+#this file exixts in sub branch
+
+name = str(input("Enter your name please:"))
+
+lenght = len(name)
+if lenght > 12:
+    print("name should not exceed 12 letters")
+
+num_or_space = name.isalpha()
+if num_or_space and lenght<=12:
+    print("validation is completed")
+elif num_or_space == False:
+    print("your name should not contains space of number!, try again")
