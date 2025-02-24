e0dc1d1ec9f55527700713a996fb6a86cb94cca0
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-24 14:32:55 +0530

    digital clock prg

--- exc.py
+++ exc.py
@@ -1,8 +1,9 @@
 #this file exixts in sub branch
 import time
-
-for i in reversed(range(1,11)):
+mytime = int(input("enter your time: "))
+for i in reversed(range (1, mytime)):
     time.sleep(1)
-    print(i)
-
-print("happy new year!!!")
+    seconds = int(i%60)
+    minutes = int(i/60)%60
+    hours = int(i/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
