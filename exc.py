8753ef919fff3d29329c4c9471439a01ecb761d8
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-25 17:18:38 +0530

    menu order prgm

--- exc.py
+++ exc.py
@@ -1,28 +1,33 @@
 #this file exixts in sub branch
 
-questions = ("how many elemnets are present in teyvat?",
-             "how many operator classes are availabe in delta force?",
-             "what is my highest kill in delta force?",
-             "what is the highest damage i hit in genshin?")
-options = (("A. 9","B. 7","C. 8","D. 6"),
-           ("A. 4","B. 6","C. 5","D. 8"),
-           ("A. 100","B. 89","C. 75","D. 68"),
-           ("A. 150k","B. 700k","C. 850k", "D. 1m"))
-answers = ("B","A","B","B")
-guesses = []
-score = 0
-qnn = 0
+menu = {"popcorn":160,
+        "drinks":190,
+        "fries":150,
+        "water":60,
+        "ice":130,
+        "sandwich":130}
 
-for qn in questions:
-    print(qn)
-    for opn in options[qnn]:
-        print(opn)
-    guess = input("enter an option [A/B/C/D]: ")
-    guesses.append(guess.upper())
-    if guess == answers:
-        print("correct")
-        score += 1
-    else:
-        print("incorrect")
-        print(f"option {answers[qnn]} is the correct answer")
-    qnn += 1
+cart = []
+total = 0
+items = menu.items()
+
+print("------ M E N U ------")
+for key, value in items:
+    print(f"{key:<10} rs:{value}/-")
+print("make sure to to type correctly")
+
+while True:
+    food = input("type in what you need or press q to quit: ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+    elif menu.get(food) is None:
+        print(f"sorry, {food} is not availabe")
+
+for food in cart:
+    print(food)
+    total += menu.get(food)
+
+print()
+print(f"your total is {total}")
