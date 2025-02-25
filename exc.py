8a2d1bd0b4ed2e5090aa1f98ce07fe18d988f618
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-25 19:56:45 +0530

    number guessing

--- exc.py
+++ exc.py
@@ -1,2 +1,24 @@
 #this file exists in main branch
+import random
 
+guess = int(input("enter a number from 1 to 10: "))
+print(f"your guess is -> {guess}")
+start = 1
+end = 10
+result = random.randint(start, end)
+print(f"the result is -> {result}")
+
+
+while True:
+    if guess > result:
+        print(f"your guess -> {guess} and our result -> {result}")
+        print("your guess if bigger than the result")
+        guess = int(input("enter a number from 1 to 10: "))
+    elif guess < result:
+        print(f"your guess -> {guess} and our result -> {result}")
+        print("your guess is smaller than the result")
+        guess = int(input("enter a number from 1 to 10: "))
+    elif guess == result:
+        print(f"your guess -> {guess} and our result -> {result}")
+        print("wow, your guess is perfect")
+        break
