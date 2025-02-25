d0dfa7252a44906fa27e05758015164e9a1e5137
manoj-k-317 <kumarmanoj317374@gmail.com>
2025-02-25 12:38:01 +0530

    quiz - failed

--- exc.py
+++ exc.py
@@ -1,12 +1,28 @@
 #this file exixts in sub branch
 
-row_1 = ["1","2","3"]
-row_2 = ["4","5","6"]
-row_3 = ["7","8","9"]
-row_4 = ["#","0","*"]
+questions = ("how many elemnets are present in teyvat?",
+             "how many operator classes are availabe in delta force?",
+             "what is my highest kill in delta force?",
+             "what is the highest damage i hit in genshin?")
+options = (("A. 9","B. 7","C. 8","D. 6"),
+           ("A. 4","B. 6","C. 5","D. 8"),
+           ("A. 100","B. 89","C. 75","D. 68"),
+           ("A. 150k","B. 700k","C. 850k", "D. 1m"))
+answers = ("B","A","B","B")
+guesses = []
+score = 0
+qnn = 0
 
-num_pad = [row_1, row_2, row_3, row_4]
-for row in num_pad:
-    for num in row:
-        print(num, end=" ")
-    print()
+for qn in questions:
+    print(qn)
+    for opn in options[qnn]:
+        print(opn)
+    guess = input("enter an option [A/B/C/D]: ")
+    guesses.append(guess.upper())
+    if guess == answers:
+        print("correct")
+        score += 1
+    else:
+        print("incorrect")
+        print(f"option {answers[qnn]} is the correct answer")
+    qnn += 1
